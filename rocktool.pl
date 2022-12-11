56286a71a7a1e146885b3d7f86fa3e656c8b9269
Gene Boggs <gene@ology.net>
2022-12-11 10:55:16 -0800

    Syntax

--- rocktool.pl
+++ rocktool.pl
@@ -106,7 +106,7 @@ __DATA__
       </div>
       <div class="col">
         <input type="text" class="form-control form-control-sm" id="parts" name="parts" value="<%= $parts %>" title="Chord progression parts" aria-describedby="partsHelp">
-        <small id="partsHelp" class="form-text text-muted">Form: &ltNote>&lt;Major|minor>&lt;verse|chorus></small>
+        <small id="partsHelp" class="form-text text-muted">Form: &lt;Note>&lt;Major|minor>&lt;verse|chorus></small>
       </div>
     </div>
   </div>
