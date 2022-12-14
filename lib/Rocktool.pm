e13f591490c63312d4752d21f07db7a9350f2473
Gene Boggs <gene@ology.net>
2022-12-13 18:38:48 -0800

    Fix weights & groups

--- lib/Rocktool.pm
+++ lib/Rocktool.pm
@@ -90,10 +90,10 @@ sub bass {
         my $groups  = [ split /[\s,]+/, $self->my_groups ];
 
         my $mdp = Music::Duration::Partition->new(
-            size    => $self->drummer->beats,
-            pool    => $pool,
-            weights => $weights,
-            groups  => $groups,
+            size => $self->drummer->beats,
+            pool => $pool,
+            $self->my_weights ? (weights => $weights) : (),
+            $self->my_groups  ? (groups  => $groups)  : (),
         );
         my @motifs = map { $mdp->motif } 1 .. $self->bass_motifs;
 
