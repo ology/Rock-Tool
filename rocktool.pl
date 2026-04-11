147d27897b3ccc483a068ea26ed292c2217b7580
Gene Boggs <gene.boggs@gmail.com>
2026-04-11 02:41:51 -0700

    WIP

--- rocktool.pl
+++ rocktool.pl
@@ -121,7 +121,7 @@ __DATA__
 % title 'Rock Progression Practice Tool';
 
 <div class="row">
-  <div class="col-6">
+  <div class="col-4">
 
 <p></p>
 
@@ -131,11 +131,9 @@ __DATA__
     <div class="row">
       <div class="col">
         <label for="parts">Parts:</label>
-        <small class="form-text text-muted">Space-separated list</small>
       </div>
       <div class="col">
-        <input type="text" class="form-control form-control-sm" id="parts" name="parts" value="<%= $parts %>" title="Space-separated chord progression parts" aria-describedby="partsHelp">
-        <small id="partsHelp" class="form-text text-muted">&lt;Note>&lt;Major|minor>&lt;verse|chorus></small>
+        <input type="text" class="form-control form-control-sm" id="parts" name="parts" value="<%= $parts %>" title="Space-separated chord progression: &lt;Note&gt;&lt;Major|minor&gt;&lt;verse|chorus&gt;">
       </div>
     </div>
   </div>
